03decbff9c17bc3b4de1f47ed77dede415112858
ivan <ivan@debian.debian>
2026-05-21 23:45:20 +0700

    Fix nomor: 6287877943496

--- render.js
+++ render.js
@@ -7,7 +7,7 @@ const qrcode = require('qrcode');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const PHONE = process.env.PHONE || '6285921368969';
+const PHONE = process.env.PHONE || '6287877943496';
 
 // Hapus auth lama biar fresh
 const authDir = './auth_render';
